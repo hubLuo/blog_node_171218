0f1fe9f5fbc45811b70da023829f2092fb1ffca1
hubLuo <hubluo@126.com>
2018-05-17 17:15:29 +0800

    用户管理：加入一般会员的评论浏览页

--- app.js
+++ app.js
@@ -19,18 +19,20 @@ app.set("view engine","ejs");
 app.use(express.static("./public"));
 
 var db=require("./config/db.js");
-//前端模块
+/*//前端模块
 app.get("/",function(req,res){
    res.send("欢迎来到我的博客");
-});
+});*/
 //后端模块
 app.use("/admin",require("./router/admin"));
 //引入用户注册和登录路由模块
 app.use(require("./router/user/users.js"));
+//前端展示模块
+app.use(require("./router/frontend/index.js"));
 //会员模块
 app.get("/member",function(req,res){
    res.send("hello member");
 });
 //注意1 use 和 get 方法的区别
-app.listen(3001);//监听端口
+app.listen(3000);//监听端口
 
